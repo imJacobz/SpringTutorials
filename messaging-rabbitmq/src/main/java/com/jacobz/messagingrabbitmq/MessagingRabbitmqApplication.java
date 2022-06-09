86e973c60f4f7366d52e595dfd609b89c4928440
realJacob <jheng@users.noreply.github.com>
2022-06-09 10:38:14 +0800

    rabbitmq

---
+++ messaging-rabbitmq/src/main/java/com/jacobz/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.jacobz.messagingrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingRabbitmqApplication.class, args);
+    }
+
+}
